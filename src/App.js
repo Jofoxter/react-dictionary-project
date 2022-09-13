c42096d64c0b5466cd80c03ff93dcf14379b4c9b
Giorgia <jofoxter@gmail.com>
2022-09-13 16:42:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./GC_Logo_Web_FullCol-13.png"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          className="App-logo img-fluid"
+          alt="logo"  />
+      </header>
+    </div>
+  );
+}
+
+
