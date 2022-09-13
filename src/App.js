952fac3d5f5536f5653dc831e8f9d4532f84f118
Giorgia <jofoxter@gmail.com>
2022-09-13 18:56:20 +0200

    Search Engine set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import logo from "./GC_Logo_Web_FullCol-13.png"
+import logo from "./GC_Logo_Vertical_FullCol.png"
+import Dictionary from "./Dictionary.js";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"  />
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center">
+            <footer className="App-footer">
+              <small>
+                Coded by{" "}
+                <a
+                  href="https://polite-cassata-f5e2ec.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Giorgia Francesca Costantino
+                </a>{" "}
+                and{" "}
+                <a
+                  href="https://github.com/Jofoxter/react-dictionary-project"
+                  ttarget="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  open source on GitHub
+                </a>
+              </small>
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
