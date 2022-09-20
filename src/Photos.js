3dca385278cf8c0f870f41e76e88490eef87c685
Giorgia <jofoxter@gmail.com>
2022-09-20 22:21:48 +0200

    img update

--- src/Photos.js
+++ src/Photos.js
@@ -8,13 +8,21 @@ export default function Photos(props){
                 <div className="row">
                   {props.photos.map(function (photo, index) {
                     return (
-                    <div className="col-4" key={index}>
-                    <a href={photo.src.original}
-                      target="_blank"            rel="noreferrer">
-                        <img src={photo.src.landscape}           className="img-fluid"
-                        rel="noreferrer" />     </a>
-                                </div>
-                              );
+                      <div className="col-4" key={index}>
+                        <a
+                          href={photo.src.original}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <img
+                            src={photo.src.landscape}
+                            className="img-fluid"
+                            rel="noreferrer"
+                            alt="visual explanaction of what you search in the dictionary"
+                          />{" "}
+                        </a>
+                      </div>
+                    );
                   })}
                 </div>
               </section>
