e1a5101c820e4c773cc6b89d5f52ec581833d240
Giorgia <jofoxter@gmail.com>
2022-09-20 16:08:35 +0200

    rel="noreferrer" updated

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,13 +4,12 @@ import React from "react";
 export default function Phonetic(props) {
 console.log(props.phonetic);
     return (
-<div className="Phonetic">
-<a href={props.phonetic.audio}
- target="_blank">
-Listen
-</a>
-<br />
-{props.phonetic.text}
-</div>
-  );
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
 }
